17e47753de93761ed0dcd46c6ac0be0b09f94b93
marques0105-beep <joao23.goncalomarques0105@gmail.com>
2026-01-10 22:29:23 +0000

    Update Simulation.java

--- src/controller/Simulation.java
+++ src/controller/Simulation.java
@@ -1,48 +1,53 @@
 package controller;
 
-import model.Road;
-import model.TrafficLight;
-import model.Vehicle;
+import model.*;
+import util.Metrics;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Simulation {
 
-    private List<Road> roads = new ArrayList<>();
-    private List<TrafficLight> lights = new ArrayList<>();
     private List<Vehicle> vehicles = new ArrayList<>();
+    private List<TrafficLight> lights = new ArrayList<>();
+    private Metrics metrics = new Metrics();
 
-    public void addRoad(Road r) {
-        roads.add(r);
-    }
+    private boolean running = false;
+    private double speedMultiplier = 1.0;
 
-    public void addLight(TrafficLight l) {
-        lights.add(l);
-    }
+    public void tick(double deltaTime) {
+        double adjusted = deltaTime * speedMultiplier;
 
-    public void addVehicle(Vehicle v) {
-        vehicles.add(v);
-    }
+        for (TrafficLight l : lights)
+            l.update(adjusted);
 
-    public List<Road> getRoads() {
-        return roads;
-    }
-
-    public List<TrafficLight> getLights() {
-        return lights;
-    }
-
-    public List<Vehicle> getVehicles() {
-        return vehicles;
+        for (Vehicle v : vehicles)
+            v.update(adjusted, metrics);
     }
 
     public void update(double deltaTime) {
-        for (TrafficLight light : lights) {
-            light.update(deltaTime);
-        }
-        for (Vehicle v : vehicles) {
-            v.update(deltaTime);
-        }
+        if (running) tick(deltaTime);
     }
+
+    public void start() { running = true; }
+    public void stop() { running = false; }
+
+    public void reset() {
+        vehicles.forEach(Vehicle::reset);
+        lights.forEach(TrafficLight::reset);
+        metrics.reset();
+    }
+
+    public void setSpeedMultiplier(double speed) {
+        this.speedMultiplier = speed;
+    }
+
+    public void addVehicle(Vehicle v) { vehicles.add(v); }
+    public void addLight(TrafficLight l) { lights.add(l); }
+
+    public List<Vehicle> getVehicles() { return vehicles; }
+    public List<TrafficLight> getLights() { return lights; }
+    public Metrics getMetrics() { return metrics; }
 }
+
+
