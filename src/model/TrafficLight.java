b83574910ce66078c9cb2a018d54924c58e3eb4e
marques0105-beep <joao23.goncalomarques0105@gmail.com>
2026-01-10 22:25:22 +0000

    Update TrafficLight.java

--- src/model/TrafficLight.java
+++ src/model/TrafficLight.java
@@ -6,51 +6,35 @@ import model.state.RedState;
 
 public class TrafficLight {
 
-    private String id;
-    private boolean red;
-
     private LightState state;
     private Strategy strategy;
 
-    public TrafficLight(String id, Strategy strategy) {
-        this.id = id;
+    public TrafficLight(Strategy strategy) {
         this.strategy = strategy;
         this.state = new RedState();
-        this.state.enter(this);
     }
 
     public void update(double deltaTime) {
         state.update(this, deltaTime);
     }
 
-    public void setState(LightState newState) {
-        this.state = newState;
-        newState.enter(this);
-    }
-
-    public boolean isRed() {
-        return red;
-    }
-
-    public void setRed(boolean r) {
-        this.red = r;
-    }
-
-    // “É permitido avançar?”
-    public boolean isGreenAllowed() {
-        // Vermelho = proibido; amarelo + verde = permitido
-        return !red;
+    public void setState(LightState state) {
+        this.state = state;
     }
 
     public Strategy getStrategy() {
         return strategy;
     }
 
-    public String getId() {
-        return id;
+    public boolean isRed() {
+        return state.getColor().equals("RED");
     }
 
-    public String getStateName() {
-        return state.getName();
+    public String getColor() {
+        return state.getColor();
+    }
+
+    public void reset() {
+        state = new RedState();
     }
 }
