3154f4c770ff0cf63a62f4dd858497bd0bd3f4ba
marques0105-beep <joao23.goncalomarques0105@gmail.com>
2026-01-10 22:25:56 +0000

    Update Road.java

--- src/model/Road.java
+++ src/model/Road.java
@@ -1,84 +1,35 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Road {
 
-    private String name;
-    private double length;
-
-    private double startX;
-    private double startY;
-
-    // Posição ao longo da estrada onde está a interseção (meio da estrada)
-    private double intersectionPosition;
-
+    private Queue<Vehicle> queue = new LinkedList<>();
     private TrafficLight trafficLight;
 
-    // Fila de veículos (para estratégias adaptativas)
-    private List<Vehicle> queue = new ArrayList<>();
-
-    public Road(String name, double length) {
-        this.name = name;
-        this.length = length;
-        this.intersectionPosition = length / 2.0;
+    public Road(TrafficLight trafficLight) {
+        this.trafficLight = trafficLight;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public double getLength() {
-        return length;
-    }
-
-    public void setStart(double x, double y) {
-        this.startX = x;
-        this.startY = y;
-    }
-
-    public double getStartX() {
-        return startX;
-    }
-
-    public double getStartY() {
-        return startY;
-    }
-
-    public double getIntersectionPosition() {
-        return intersectionPosition;
-    }
-
-    public void setIntersectionPosition(double intersectionPosition) {
-        this.intersectionPosition = intersectionPosition;
-    }
-
-    public void setTrafficLight(TrafficLight light) {
-        this.trafficLight = light;
-    }
-
-    public TrafficLight getTrafficLight() {
-        return trafficLight;
-    }
-
-    // ===== Fila de veículos =====
-
-    public void addToQueue(Vehicle v) {
-        if (!queue.contains(v)) {
-            queue.add(v);
-        }
-    }
-
-    public void removeFromQueue(Vehicle v) {
-        queue.remove(v);
+    public void addVehicle(Vehicle v) {
+        queue.add(v);
     }
 
     public int getQueueSize() {
         return queue.size();
     }
 
-    public List<Vehicle> getQueue() {
-        return queue;
+    public boolean hasEmergencyVehicle() {
+        return queue.stream().anyMatch(Vehicle::isEmergency);
+    }
+
+    public boolean hasRedLight() {
+        return trafficLight.isRed();
+    }
+
+    public TrafficLight getTrafficLight() {
+        return trafficLight;
     }
 }
+
