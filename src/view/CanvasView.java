2166c44a39faeb32094a5706221103b4f0876ae1
marques0105-beep <joao23.goncalomarques0105@gmail.com>
2026-01-11 14:38:21 +0000

    Update CanvasView.java

--- src/view/CanvasView.java
+++ src/view/CanvasView.java
@@ -4,24 +4,70 @@ import controller.Simulation;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import model.Intersection;
 import model.Vehicle;
 
-public class CanvasView {
+public class CanvasView extends Canvas {
 
-    private Canvas canvas;
+    private Simulation simulation;
     private GraphicsContext gc;
 
-    public CanvasView(Canvas canvas) {
-        this.canvas = canvas;
-        this.gc = canvas.getGraphicsContext2D();
+    public CanvasView(Simulation simulation) {
+        super(800, 400);
+        this.simulation = simulation;
+        this.gc = getGraphicsContext2D();
     }
 
-    public void render(Simulation sim) {
-        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+    public void render() {
+        gc.clearRect(0, 0, getWidth(), getHeight());
 
-        for (Vehicle v : sim.getVehicles()) {
-            gc.setFill(v.isEmergency() ? Color.RED : Color.BLUE);
-            gc.fillRect(v.getPosition(), 200, 30, 15);
+        drawRoad();
+        drawVehicles();
+        drawTrafficLights();
+        drawStats();
+    }
+
+
+    private void drawRoad() {
+        gc.setFill(Color.DARKGRAY);
+        gc.fillRect(0, 180, 800, 40);
+    }
+
+    private void drawVehicles() {
+        gc.setFill(Color.BLUE);
+
+        for (Vehicle v : simulation.getRoad().getVehicles()) {
+            gc.fillRect(v.getPosition(), 185, 20, 30);
         }
+    }
+
+    private void drawTrafficLights() {
+
+        for (Intersection i : simulation.getIntersections()) {
+
+            String state = i.getTrafficLight().getStateName();
+            Color color;
+
+            switch (state) {
+                case "GREEN" -> color = Color.GREEN;
+                case "YELLOW" -> color = Color.YELLOW;
+                default -> color = Color.RED;
+            }
+
+            gc.setFill(color);
+            gc.fillOval(i.getPosition(), 120, 30, 30);
+
+            gc.setFill(Color.BLACK);
+            gc.fillText(state, i.getPosition(), 110);
+        }
+    }
+
+
+    private void drawStats() {
+        gc.setFill(Color.BLACK);
+        gc.fillText(
+                "Waiting cars: " + simulation.countWaitingCars(),
+                10, 20
+        );
     }
 }
