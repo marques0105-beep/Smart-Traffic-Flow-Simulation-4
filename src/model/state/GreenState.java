da51dd25e02c617923cedb7bc8e0268b1ab362ed
marques0105-beep <joao23.goncalomarques0105@gmail.com>
2026-01-11 14:35:42 +0000

    Update GreenState.java

--- src/model/state/GreenState.java
+++ src/model/state/GreenState.java
@@ -4,20 +4,23 @@ import model.TrafficLight;
 
 public class GreenState implements LightState {
 
-    private double timer = 0;
-
     @Override
-    public void update(TrafficLight light, double deltaTime) {
-        timer += deltaTime;
-        if (timer >= light.getStrategy().getGreenDuration()) {
-            light.setState(new YellowState());
+    public void update(TrafficLight light, double dt) {
+        if (light.timerExpired()) {
+            light.markGreenFinished();
+            light.changeState(new YellowState());
         }
     }
 
+
     @Override
-    public String getColor() {
+    public boolean canPass() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
         return "GREEN";
     }
 }
-
 
