76f558b1b6250c64fa3c5c7ba64a9f9840d620a4
marques0105-beep <joao23.goncalomarques0105@gmail.com>
2026-01-11 14:37:49 +0000

    Update CsvExporter.java

--- src/util/CsvExporter.java
+++ src/util/CsvExporter.java
@@ -5,11 +5,10 @@ import java.io.IOException;
 
 public class CsvExporter {
 
-    public static void export(String file, Metrics metrics) {
-        try (FileWriter writer = new FileWriter(file)) {
-            writer.write("servedCars,averageWaitingTime\n");
-            writer.write(metrics.getServedCars() + "," +
-                    metrics.getAverageWaitingTime());
+    public static void export(Metrics m) {
+        try (FileWriter fw = new FileWriter("stats.csv")) {
+            fw.write("cars_served,average_wait\n");
+            fw.write(m.getCarsServed() + "," + m.getAverageWait());
         } catch (IOException e) {
             e.printStackTrace();
         }
