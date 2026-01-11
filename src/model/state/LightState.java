7177c1fe62ba77d20d9abb82f3ecf4353ed04123
marques0105-beep <joao23.goncalomarques0105@gmail.com>
2026-01-11 14:35:52 +0000

    Update LightState.java

--- src/model/state/LightState.java
+++ src/model/state/LightState.java
@@ -3,6 +3,7 @@ package model.state;
 import model.TrafficLight;
 
 public interface LightState {
-    void update(TrafficLight light, double deltaTime);
-    String getColor();
+    void update(TrafficLight light, double dt);
+    boolean canPass();
+    String getName();
 }
